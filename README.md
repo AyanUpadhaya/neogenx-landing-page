333f17fc384e76f4ed6e7e99a88b4120700051bb
Ayan Updhaya <ayanU881@gmail.com>
2023-03-18 23:01:38 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# neogenx-landing-page
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      sm:"468px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px",
+    },
+    extend: {
+      colors:{
+        brightRed:'hsl(12,80%,59%)',
+        brightRedLight:'hsl(12,80%,69%)',
+        darkBlue:'hsl(228,39%,23%)',
+        brightRedSupLight:'hsl(12,88%,95%)',
+      }
+    },
+  },
+  plugins: [],
+}
